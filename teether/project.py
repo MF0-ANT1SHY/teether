3ab4f29005bca6d49957a6d91522c2168d9d9c17
surey <komeijiddz@gmail.com>
2024-11-10 00:22:43 +0800

    format output

--- teether/project.py
+++ teether/project.py
@@ -25,6 +25,8 @@ def load_json(path):
 
 class Project(object):
     def __init__(self, code, cfg=None):
+        self.filename = ''
+        self.starttime = 0
         self.name = "default"
         self.code = code
         self._prg = None
